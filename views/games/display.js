bc3361befae2d22be14c36b91e0e00e63e55dbf8
Joe McDonough <2jomcd@gmail.com>
2014-12-16 17:15:13 -0800

    Tues 5pm push

--- views/games/display.js
+++ views/games/display.js
@@ -1,35 +1,65 @@
 $(document).ready(function() {
-	// var xPic = "url(views/picsLib/moose/images(1).jpeg)";
-	// var oPic = "url(views/picsLib/shoe/images(1).jpeg)";
-	var picpre = "url(picsLib/mix/im";
+var turn = 0;
+
+var picpre = "url(picsLib/mix/im";
+var picsuf = ")";
+
+var choosepic = function()	{
+	var randnum = Math.floor((Math.random() * 20) + 1);
+	var APic = picpre+randnum+picsuf;
+	return APic
+};
+
+$('#a1').css('background-image', choosepic());
+$('#a2').css('background-image', choosepic());
+$('#a3').css('background-image', choosepic());
+$('#a4').css('background-image', choosepic());
+$('#a5').css('background-image', choosepic());
+$('#a6').css('background-image', choosepic());
+$('#a7').css('background-image', choosepic());
+$('#a8').css('background-image', choosepic());
+$('#a9').css('background-image', choosepic());
+
+// load grid1 with 9 images
+	// $(".av").click(function() {
+	// 	var picp = "url(picsLib/mix/im";
+	// 	var picnumY = Math.floor((Math.random() * 20) + 1);
+	// 	var pics = ")";
+	// 	var yPic = picp+picnumY+pics;
+	// 	$(this).css("background", yPic);
+	// });
+
 	var picnumX = Math.floor((Math.random() * 20) + 1);
 	var picnumO = Math.floor((Math.random() * 20) + 1);
-	var picsuf = ")";
 	var xPic = picpre+picnumX+picsuf;
 	var oPic = picpre+picnumO+picsuf;
-	var turn = 0; 
-	// console.log("t1  " + turn);
-	// $("#dogbutton").click(function(e) {
-	// 	turn = 0;
-	// });
-	// $("#catbutton").click(function(e) {
-	// 	turn = 1;
-	// });
+
+var player_count = 0;
+
+$(".av").click(function(e)  {
+	player_count+=1;
+	if (player_count === 1) {
+		xPic = $(this).css('background-image');
+	}
+	else if (player_count > 1)  {
+		oPic = $(this).css('background-image');
+	}
+	return xPic, oPic;
+});
+
 
 // display images
 	$(".cell").click(function(e) {
 		if (turn===0) {
-			$(this).css("background", xPic);
+			$(this).css('background-image', xPic);
 			turn = 1;
 		} 
 		else {
-			$(this).css("background", oPic);
+			$(this).css('background-image', oPic);
 			turn = 0;
 		};
 	});
 
-// reset on reset button click
-	// $("#resetbutton").click(function(e) {location.reload()});
 
 });
 
