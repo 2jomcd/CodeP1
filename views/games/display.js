0c96abb8cfcb221012336cb62437bd16657f8dac
Joe McDonough <2jomcd@gmail.com>
2014-12-16 17:30:41 -0800

    Tues 530 changes to display.js

--- views/games/display.js
+++ views/games/display.js
@@ -3,11 +3,10 @@ var turn = 0;
 
 var picpre = "url(picsLib/mix/im";
 var picsuf = ")";
-
 var choosepic = function()	{
 	var randnum = Math.floor((Math.random() * 20) + 1);
-	var APic = picpre+randnum+picsuf;
-	return APic
+	var aPic = picpre+randnum+picsuf;
+	return aPic
 };
 
 $('#a1').css('background-image', choosepic());
@@ -20,46 +19,45 @@ $('#a7').css('background-image', choosepic());
 $('#a8').css('background-image', choosepic());
 $('#a9').css('background-image', choosepic());
 
-// load grid1 with 9 images
-	// $(".av").click(function() {
-	// 	var picp = "url(picsLib/mix/im";
-	// 	var picnumY = Math.floor((Math.random() * 20) + 1);
-	// 	var pics = ")";
-	// 	var yPic = picp+picnumY+pics;
-	// 	$(this).css("background", yPic);
-	// });
+// // load grid1 with 9 images
+// 	// $(".av").click(function() {
+// 	// 	var picp = "url(picsLib/mix/im";
+// 	// 	var picnumY = Math.floor((Math.random() * 20) + 1);
+// 	// 	var pics = ")";
+// 	// 	var yPic = picp+picnumY+pics;
+// 	// 	$(this).css("background", yPic);
+// 	// });
 
-	var picnumX = Math.floor((Math.random() * 20) + 1);
-	var picnumO = Math.floor((Math.random() * 20) + 1);
-	var xPic = picpre+picnumX+picsuf;
-	var oPic = picpre+picnumO+picsuf;
+// 	var picnumX = Math.floor((Math.random() * 20) + 1);
+// 	var picnumO = Math.floor((Math.random() * 20) + 1);
+	var Pic1 = 'Pic1';
+	var Pic2 = 'Pic2';
 
+// assign pics to player1=1Pic and player2 = 2Pic
 var player_count = 0;
-
 $(".av").click(function(e)  {
+	if (player_count>1) {return};
 	player_count+=1;
 	if (player_count === 1) {
-		xPic = $(this).css('background-image');
+		Pic1 = $(this).css('background-image');
 	}
 	else if (player_count > 1)  {
-		oPic = $(this).css('background-image');
+		Pic2 = $(this).css('background-image');
 	}
-	return xPic, oPic;
+	return Pic1, Pic2;
 });
 
 
 // display images
 	$(".cell").click(function(e) {
 		if (turn===0) {
-			$(this).css('background-image', xPic);
+			$(this).css('background-image', Pic1);
 			turn = 1;
 		} 
 		else {
-			$(this).css('background-image', oPic);
+			$(this).css('background-image', Pic2);
 			turn = 0;
 		};
 	});
-
-
 });
 
